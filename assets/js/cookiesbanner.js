7920b405eac0bf829176c3639d11f5af61dfac8a
chmullerweb <ch.muller.web@gmail.com>
2020-02-20 17:35:30 +0100

    Exos JS

--- assets/js/cookiesbanner.js
+++ assets/js/cookiesbanner.js
@@ -1,7 +1,20 @@
 document.querySelector('.accept').addEventListener('click', function(){
   alert('Vous avez accepté les cookies.');
+  document.querySelector('.accept').remove();
 });
 
 document.querySelector('.refuse').addEventListener('click', function(){
   alert('Vous avez refusé les cookies.');
+});
+
+document.querySelector('img').addEventListener('mouseover', function(){
+  alert('J\'ai été survolée !');
+});
+
+document.querySelector('.bgcolor_button').addEventListener('click', function changerCouleurDeFond (couleur){
+    document.getElementById("bgcolor").style.backgroundColor='red';
+});
+
+document.querySelector('.name_button').addEventListener('click', function changerCouleurDuTexte (couleur){
+    document.getElementById("name").style.color='blue';
 });
