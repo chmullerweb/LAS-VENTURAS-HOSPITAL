2afc733f97840994b29bf72b92ef8049e130470c
chmullerweb <ch.muller.web@gmail.com>
2020-02-26 17:29:02 +0100

    Update cookiesbanner.js

--- assets/js/cookiesbanner.js
+++ assets/js/cookiesbanner.js
@@ -18,3 +18,12 @@ document.querySelector('.bgcolor_button').addEventListener('click', function cha
 document.querySelector('.name_button').addEventListener('click', function changerCouleurDuTexte (couleur){
     document.getElementById("name").style.color='blue';
 });
+
+
+// Et pour demain, vous pouvez vous entraîner sur l'exercice de "thème clair" / "thème sombre".
+// Pour cet exercice, le but est de coder la fonctionalité suivante :
+// - Il doit y avoir deux boutons en haut de la page : un bouton « Thème clair » et un bouton « Thème sombre ».
+// - Quand on clique sur le bouton « Thème clair », les textes de la page doivent s’afficher en noir sur fond blanc.
+// - Quand on clique sur le bouton « Thème sombre », les textes de la page doivent s’afficher en blanc sur fond noir.
+// Vous pouvez utiliser toutes les manières que vous voulez pour ça,
+// tant que ça fonctionne :)
